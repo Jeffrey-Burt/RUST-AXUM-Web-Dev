cce0a1824d8355ae88494f07d46ed46a56e6b851
Jeffrey Burt <jeffrey.burt10@knights.ucf.edu>
2023-12-11 18:45:48 -0800

    First POST and GET requests.

--- my_rust_api/src/main.rs
+++ my_rust_api/src/main.rs
@@ -1,11 +1,51 @@
-use axum::{routing::get, Router};
+use axum::{
+    body::Body,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Json, Router,
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    name: String,
+    email: String
+}
+
+async fn create_user() -> impl IntoResponse {
+    Response::builder()
+        .status(StatusCode::CREATED)
+        .body(Body::from("User created successfully"))
+        .unwrap()
+}
+
+async fn list_users() -> Json<Vec<User>> {
+    let users = vec![
+        User {
+            id: 1,
+            name: "Jeffrey".to_string(),
+            email: "jeffrey@email.com".to_string(),
+        },
+        User {
+            id: 2,
+            name: "Zach".to_string(),
+            email: "zach@zach.com".to_string(),
+        },
+    ];
+    Json(users)
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {"Hello, Rust!"}));
+    let app = Router::new()
+        .route("/", get(|| async {"Hello, Rust!"}))
+        .route("/create-user", post(create_user))
+        .route("/users", get(list_users));
 
     println!("Running on http://localhost:3000");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
